70ee01e2c1b527c0678f76c6413f0a008dffb789
Chubbibanana <dongsoo.tchah@gmail.com>
2021-03-28 22:30:36 -0700

    1534

---
+++ 1534.js
@@ -0,0 +1,14 @@
+var countGoodTriplets = function (arr, a, b, c) {
+    let trips = 0;
+    const n = arr.length;
+    for (let i = 0; i < n - 2; i++)
+      for (let j = i + 1; j < n - 1; j++)
+        for (let k = j + 1; k < n; k++)
+          if (
+            Math.abs(arr[i] - arr[j]) <= a &&
+            Math.abs(arr[j] - arr[k]) <= b &&
+            Math.abs(arr[i] - arr[k]) <= c
+          )
+            trips++;
+    return trips;
+};
