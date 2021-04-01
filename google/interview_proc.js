a8a84bfe92bd4f8790c90be2967e3de94745666d
Chubbibanana <dongsoo.tchah@gmail.com>
2021-03-31 19:11:46 -0700

    Unique emails

---
+++ google/interview_proc.js
@@ -0,0 +1,27 @@
+var numUniqueEmails = function (emails) {
+    let uniques = {};
+
+    for (let idx = 0; idx < emails.length; idx++){
+        // email string with . ignored, + ignore all till @
+        let returnedString = '';
+        const email = emails[idx];
+        let i = 0;
+        let index = email.indexOf('@');
+        while (i < index) {
+            if (email[i] === '+'){
+                i++;
+                while (email[i] !== '@') {
+                    i++;
+                }
+            } else if (email[i] === '.') {
+                i++;    
+            } else {
+                returnedString += email[i];
+                i++;
+            }
+        }
+        returnedString += email.slice(index);
+        uniques[returnedString] = true;
+    }
+    return Object.keys(uniques).length;
+};
