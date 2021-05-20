dc5884e440716571105f34c0320231b555375445
Chubbibanana <dongsoo.tchah@gmail.com>
2021-05-19 18:12:16 -0700

    419

--- medium/419.js
+++ medium/419.js
@@ -1,24 +1,14 @@
 var countBattleships = function (board) {
-  let battleships = 0;
+  let battleships = 0; // O(1);
 
   for (let row = 0; row < board.length; row++) {
     for (let column = 0; column < board[0].length; column++) {
-      if (board[row][column] === "X") {
+      if (
+        board[row][column] === "X" &&
+        (!board[row + 1] || board[row + 1][column] !== "X") &&
+        (!board[row][column + 1] || board[row][column + 1] !== "X")
+      ) {
         battleships++;
-        board[row][column] = ".";
-        let newRow = row + 1;
-        let newCol = column + 1;
-        //                 going down
-
-        while (newRow < board.length && board[newRow][column] === "X") {
-          board[newRow][column] = ".";
-          newRow++;
-        }
-        //                 going right
-        while (newCol < board[0].length && board[row][newCol] === "X") {
-          board[row][newCol] = ".";
-          newCol++;
-        }
       }
     }
   }
