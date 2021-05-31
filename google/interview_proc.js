6330b1a5028fb4e7de332c2d1b4a4583e6e043f7
Chubbibanana <dongsoo.tchah@gmail.com>
2021-05-31 16:31:24 -0700

    google practice

--- google/interview_proc.js
+++ google/interview_proc.js
@@ -51,3 +51,31 @@ var checkValid = function(arr) {
     }
     return (i === arr.length - 1);
 }
+
+var threeSum = function (nums) {
+  let triplets = [];
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > 0) break;
+    if (nums[i] === nums[i - 1]) continue;
+    let left = i + 1;
+    let right = nums.length - 1;
+    while (left < right) {
+      const sum = nums[i] + nums[left] + nums[right];
+
+      if (sum > 0) {
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        triplets.push([nums[i], nums[left], nums[right]]);
+        while (left < right && nums[left] === nums[left + 1]) left++;
+        while (left < right && nums[right] === nums[right - 1]) right--;
+        left++;
+        right--;
+      }
+    }
+  }
+  return triplets;
+};
